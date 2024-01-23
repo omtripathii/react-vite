46ea90cbe0aa80e404e35b1cd2e98eec97f0a51d
OM TRIPATHI <109409112+omtripathii@users.noreply.github.com>
2024-01-23 06:35:00 +0530

    Create Todo.jsx

---
+++ src/components/Todo.jsx
@@ -0,0 +1,43 @@
+
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: tasks.length + 1, text: newTask }]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => removeTask(task.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
